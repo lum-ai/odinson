8109bd55fd620fd12ba362133b110b2c3283f795
Gus Hahn-Powell <gushahnpowell@gmail.com>
2019-02-19 16:16:35 -0700

    Use .ser due to clulab/processors#310

--- extra/src/main/scala/ai/lum/odinson/extra/AnnotateText.scala
+++ extra/src/main/scala/ai/lum/odinson/extra/AnnotateText.scala
@@ -1,11 +1,12 @@
 package ai.lum.odinson.extra
 
 import java.io.File
-import org.clulab.processors.Processor
+import org.clulab.processors.{ Document, Processor }
 import org.clulab.processors.clu.{ CluProcessor, BioCluProcessor }
 import org.clulab.serialization.json._
 import com.typesafe.scalalogging.LazyLogging
 import com.typesafe.config.ConfigFactory
+import ai.lum.common.Serializer
 import ai.lum.common.ConfigUtils._
 import ai.lum.common.FileUtils._
 //import ai.lum.common.Serializer
@@ -26,7 +27,8 @@ object AnnotateText extends App with LazyLogging {
 
   // NOTE parses the documents in parallel
   for (f <- textDir.listFilesByWildcard("*.txt").toSeq.par) {
-    val docFile = new File(docsDir, f.getBaseName() + ".json")
+    //val docFile = new File(docsDir, f.getBaseName() + ".json")
+    val docFile = new File(docsDir, f.getBaseName() + ".ser")
     if (docFile.exists()) {
       logger.warn(s"${docFile.getCanonicalPath} already exists")
     } else {
@@ -35,7 +37,8 @@ object AnnotateText extends App with LazyLogging {
       val doc = processor.annotate(text)
       // use file base name as document id
       doc.id = Some(f.getBaseName())
-      doc.saveJSON(docFile, pretty = true)
+      //doc.saveJSON(docFile, pretty = true)
+      Serializer.serialize[Document](doc, docFile)
     }
   }
 
