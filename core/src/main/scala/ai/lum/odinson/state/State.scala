2444468e91ed69b04cb5bacbba9eed77ad063895
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2019-03-04 11:44:56 -0700

    mentions

--- core/src/main/scala/ai/lum/odinson/state/State.scala
+++ core/src/main/scala/ai/lum/odinson/state/State.scala
@@ -1,5 +1,6 @@
 package ai.lum.odinson.state
 
+import scala.collection.mutable.ArrayBuffer
 import org.h2.jdbcx.JdbcDataSource
 
 class State {
@@ -7,5 +8,61 @@ class State {
   val dataSource = new JdbcDataSource()
   dataSource.setURL("jdbc:h2:mem:odinson")
   val connection = dataSource.getConnection()
+  val statement = connection.createStatement()
+
+  def close(): Unit = {
+    statement.close()
+    connection.close()
+  }
+
+  def init(): Unit = {
+    val sql = """CREATE TABLE mentions (
+                |  id BIGINT AUTO_INCREMENT,
+                |  label VARCHAR(50) NOT NULL,
+                |  luceneDocId INT NOT NULL,
+                |  startToken INT NOT NULL,
+                |  endToken INT NOT NULL,
+                |);""".stripMargin
+    statement.executeUpdate(sql)
+  }
+
+  def addMention(
+    label: String,
+    luceneDocId: Int,
+    startToken: Int,
+    endToken: Int
+  ): Unit = {
+    val sql = s"""INSERT INTO mentions (
+                 |  label,
+                 |  luceneDocId,
+                 |  startToken,
+                 |  endToken,
+                 |) VALUES (
+                 |  '$label',
+                 |  $luceneDocId,
+                 |  $startToken,
+                 |  $endToken,
+                 |);""".stripMargin
+    statement.executeUpdate(sql)
+  }
+
+  def getMentions(label: String, luceneDocId: Int) = {
+    val sql = s"""SELECT *
+                 |FROM mentions
+                 |WHERE label='$label' AND luceneDocId=$luceneDocId
+                 |;""".stripMargin
+    val results = statement.executeQuery(sql)
+    val mentions = ArrayBuffer.empty[(String, Int, Int, Int)]
+    while (results.next()) {
+      val mention = (
+        results.getString("label"),
+        results.getInt("luceneDocId"),
+        results.getInt("startToken"),
+        results.getInt("endToken"),
+      )
+      mentions += mention
+    }
+    mentions.toArray
+  }
 
 }
