c1d5c3dece2b0dce1f472a56ac7cb82f60ec88f1
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2019-06-24 10:29:03 -0700

    don't discard trailing empty strings

--- core/src/test/scala/ai/lum/odinson/TestPatterns.scala
+++ core/src/test/scala/ai/lum/odinson/TestPatterns.scala
@@ -20,7 +20,7 @@ class TestPatterns extends FlatSpec with Matchers {
 
   for (line <- lines.drop(1)) { // skip header
     val Array(pattern, string, allExpected) = line.trim.split("\t")
-    val expected = allExpected.split(";")
+    val expected = allExpected.split(";", -1)
     pattern should s"find all expected results for «$string»" in {
       val ee = TestUtils.mkExtractorEngine(string)
       val results = ee.query(pattern)
