b71a007630a4403c1a788a0f81fce411da61675c
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2019-08-11 14:12:19 -0700

    started document

---
+++ core/src/main/scala/ai/lum/odinson/OdinsonDocument.scala
@@ -0,0 +1,77 @@
+package ai.lum.odinson
+
+import upickle.default._
+
+
+
+case class Document(
+  metadata: Seq[Field],
+  sentences: Seq[Sentence]
+) {
+  def toJson: String = write(this)
+  def toPrettyJson: String = write(this, indent = 4)
+}
+
+object Document {
+  implicit val rw: ReadWriter[Document] = macroRW
+  def fromJson(data: String): Document = {
+    read[Document](data)
+  }
+}
+
+
+
+case class Sentence(
+  fields: Seq[Field]
+) {
+  def toJson: String = write(this)
+  def toPrettyJson: String = write(this, indent = 4)
+}
+
+object Sentence {
+  implicit val rw: ReadWriter[Sentence] = macroRW
+  def fromJson(data: String): Sentence = {
+    read[Sentence](data)
+  }
+}
+
+
+
+sealed trait Field {
+  def name: String
+  def toJson: String = write(this)
+  def toPrettyJson: String = write(this, indent = 4)
+}
+
+object Field {
+  implicit val rw: ReadWriter[Field] = {
+    ReadWriter.merge(TokensField.rw, GraphField.rw)
+  }
+}
+
+case class TokensField(
+  name: String,
+  tokens: Seq[String],
+  store: Boolean,
+) extends Field
+
+object TokensField {
+  implicit val rw: ReadWriter[TokensField] = macroRW
+  def fromJson(data: String): TokensField = {
+    read[TokensField](data)
+  }
+}
+
+case class GraphField(
+  name: String,
+  incomingEdges: Seq[Seq[(Int, String)]],
+  outgoingEdges: Seq[Seq[(Int, String)]],
+  roots: Seq[Int],
+) extends Field
+
+object GraphField {
+  implicit val rw: ReadWriter[GraphField] = macroRW
+  def fromJson(data: String): GraphField = {
+    read[GraphField](data)
+  }
+}
