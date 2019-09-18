d12b86298716b62d4179db0f0c7de49fb170c59a
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2019-09-18 10:11:31 -0700

    added back synchronizeOrderWithDocumentId support

--- extra/src/main/scala/ai/lum/odinson/extra/IndexDocuments.scala
+++ extra/src/main/scala/ai/lum/odinson/extra/IndexDocuments.scala
@@ -1,6 +1,7 @@
 package ai.lum.odinson.extra
 
 import java.io._
+import scala.collection.GenSeq
 import scala.util.{ Try, Success, Failure }
 import com.typesafe.scalalogging.LazyLogging
 import ai.lum.common.ConfigFactory
@@ -11,23 +12,38 @@ import ai.lum.odinson.{ Document, OdinsonIndexWriter }
 object IndexDocuments extends App with LazyLogging {
 
   val config = ConfigFactory.load()
-  val docsDir  = config[File]("odinson.docsDir")
+  val docsDir = config[File]("odinson.docsDir")
+  val synchronizeOrderWithDocumentId = config[Boolean]("odinson.index.synchronizeOrderWithDocumentId")
 
-  val writer = OdinsonIndexWriter.fromConfig
+  val writer = OdinsonIndexWriter.fromConfig()
 
-  // NOTE indexes the documents in parallel
-  for {
-    f <- docsDir.listFilesByWildcard("*.json", recursive = true).toSeq.par
-  } {
+  // a sequence of (File, Document) tuples that *may* be parallel
+  val filesAndDocs: GenSeq[(File, Document)] =
+    if (synchronizeOrderWithDocumentId) {
+      // files ordered by the id of the document
+      docsDir.listFilesByWildcard("*.json", recursive = true)
+        .map(f => (f, Document.fromJson(f)))
+        .toSeq
+        // NOTE we convert to int to preserve functionality added in PR #37
+        // but maybe we should sort lexicographically instead
+        .sortBy(_._2.id.toInt)
+    } else {
+      docsDir.listFilesByWildcard("*.json", recursive = true)
+        .map(f => (f, Document.fromJson(f)))
+        .toSeq.par // make parallel sequence
+    }
+
+  // index documents
+  for (fileWithDoc <- filesAndDocs) {
+    val (file, doc) = fileWithDoc
     Try {
-      val doc = Document.fromJson(f)
       val block = writer.mkDocumentBlock(doc)
       writer.addDocuments(block)
     } match {
       case Success(_) =>
-        logger.info(s"Indexed ${f.getName}")
+        logger.info(s"Indexed ${file.getName}")
       case Failure(e) =>
-        logger.error(s"Failed to index ${f.getName}", e)
+        logger.error(s"Failed to index ${file.getName}", e)
     }
 
   }
