6e793a5aa9d2528a0b51830b1a36a0d5e28a87b6
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2019-09-23 19:29:09 -0700

    don't index repeated dependency labels

--- core/src/main/scala/ai/lum/odinson/lucene/analysis/DependencyTokenStream.scala
+++ core/src/main/scala/ai/lum/odinson/lucene/analysis/DependencyTokenStream.scala
@@ -15,38 +15,40 @@ class DependencyTokenStream(val edges: Array[Array[(Int, String)]]) extends Toke
   val termAtt = addAttribute(classOf[CharTermAttribute])
 
   private var tokenIndex: Int = 0
-  private var edgeIndex: Int = -1
+  private var labelIndex: Int = -1
   private var positionIncrement: Int = 1
 
   @tailrec
   final def incrementToken(): Boolean = {
     clearAttributes()
     if (tokenIndex >= edges.length) return false
-    val tokenEdges = edges(tokenIndex)
-    edgeIndex += 1
-    if (edgeIndex < tokenEdges.length) {
-      if (edgeIndex == 0) {
-        // Increment position for the first edge of the current token.
+    // Get labels of edges corresponding to current token.
+    // Don't repeat labels.
+    val labels = edges(tokenIndex).map(_._2).distinct
+    labelIndex += 1
+    if (labelIndex < labels.length) {
+      if (labelIndex == 0) {
+        // Increment position for the first label of the current token.
         posIncrAtt.setPositionIncrement(positionIncrement)
         positionIncrement = 1
       } else {
-        // Store all edges for the current token in the same position.
+        // Store all labels for the current token in the same position.
         posIncrAtt.setPositionIncrement(0)
       }
       // Add edge label.
-      val label = tokenEdges(edgeIndex)._2
+      val label = labels(labelIndex)
       termAtt.setEmpty()
       termAtt.append(label)
       true
-    } else { // No more edges for the current token.
-      if (tokenEdges.isEmpty) {
-        // If the current token has no edges then we need to include it
+    } else { // No more labels for the current token.
+      if (labels.isEmpty) {
+        // If the current token has no labels then we need to include it
         // in `positionIncrement` so that the next increment is correct.
         positionIncrement += 1
       }
       // Go to the next token.
       tokenIndex += 1
-      edgeIndex = -1
+      labelIndex = -1
       incrementToken()
     }
   }
@@ -54,7 +56,7 @@ class DependencyTokenStream(val edges: Array[Array[(Int, String)]]) extends Toke
   override def reset(): Unit = {
     super.reset()
     tokenIndex = 0
-    edgeIndex = -1
+    labelIndex = -1
     positionIncrement = 1
   }
 
