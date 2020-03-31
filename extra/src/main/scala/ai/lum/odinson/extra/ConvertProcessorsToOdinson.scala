0c0f55797c40998452de7bd85e3a591b26c68506
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2020-03-31 11:58:59 -0700

    convert

---
+++ extra/src/main/scala/ai/lum/odinson/extra/ConvertProcessorsToOdinson.scala
@@ -0,0 +1,35 @@
+package ai.lum.odinson.extra
+
+import java.io.File
+import scala.util.{ Try, Success, Failure }
+import com.typesafe.scalalogging.LazyLogging
+import org.clulab.serialization.json.JSONSerializer
+import ai.lum.common.ConfigUtils._
+import ai.lum.common.ConfigFactory
+import ai.lum.common.FileUtils._
+import ai.lum.odinson.Document
+
+object ConvertProcessorsToOdinson extends App with LazyLogging {
+
+  val config = ConfigFactory.load()
+  val docsDir = config[File]("odinson.docsDir")
+  val procDir = config[File]("odinson.procDir")
+
+  logger.info(s"processors documents at $procDir")
+  logger.info(s"odinson documents at $docsDir")
+
+  for (f <- procDir.listFilesByWildcard("*.json", recursive = true).toSeq.par) {
+    Try {
+      val newFile = new File(docsDir, f.getName + ".gz")
+      val processorsDoc = JSONSerializer.toDocument(f)
+      val odinsonDoc = ProcessorsUtils.convertDocument(processorsDoc)
+      newFile.writeString(odinsonDoc.toJson)
+    } match {
+      case Success(_) =>
+        logger.info(s"Converted ${f.getName}")
+      case Failure(e) =>
+        logger.error(s"Failed to convert ${f.getName}", e)
+    }
+  }
+
+}
