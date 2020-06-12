415ee644be797d0a2d49a6f285f1b781b60334a1
gcgbarbosa <gcgbarbosa@gmail.com>
2020-06-12 14:32:25 -0700

    Add base class

---
+++ core/src/test/scala/ai/lum/odinson/BaseSpec.scala
@@ -0,0 +1,21 @@
+package ai.lum.odinson
+
+/*
+import org.apache.lucene.index.DirectoryReader
+import org.apache.lucene.document.{ Document => LuceneDocument, _ }
+import org.apache.lucene.document.Field.Store
+import com.typesafe.config.Config
+import ai.lum.common.ConfigFactory
+import ai.lum.common.ConfigUtils._
+import ai.lum.odinson.lucene._
+import ai.lum.odinson.lucene.search._
+import ai.lum.odinson.compiler.QueryCompiler
+import ai.lum.odinson.state.State
+*/
+// tests stuff
+import org.scalatest._
+
+class BaseSpec extends FlatSpec with Matchers  {
+
+
+}
