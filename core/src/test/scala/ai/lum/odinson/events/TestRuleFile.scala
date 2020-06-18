3d770100ef521347b0bd6fca895b4e1a13ed934f
gcgbarbosa <gcgbarbosa@gmail.com>
2020-06-18 11:02:36 -0700

    Move TestRuleFile & use new eventspec

--- core/src/test/scala/ai/lum/odinson/events/TestRuleFile.scala
+++ core/src/test/scala/ai/lum/odinson/events/TestRuleFile.scala
@@ -1,11 +1,8 @@
-package ai.lum.odinson.foundations
+package ai.lum.odinson.events
 
 import org.scalatest._
 
-import ai.lum.odinson.BaseSpec
-import ai.lum.odinson.events.TestEvents._
-
-class TestRuleFile extends BaseSpec {
+class TestRuleFile extends EventSpec{
   val json = """{"id":"56842e05-1628-447a-b440-6be78f669bf2","metadata":[],"sentences":[{"numTokens":5,"fields":[{"$type":"ai.lum.odinson.TokensField","name":"raw","tokens":["Becky","ate","gummy","bears","."],"store":true},{"$type":"ai.lum.odinson.TokensField","name":"word","tokens":["Becky","ate","gummy","bears","."]},{"$type":"ai.lum.odinson.TokensField","name":"tag","tokens":["NNP","VBD","JJ","NNS","."]},{"$type":"ai.lum.odinson.TokensField","name":"lemma","tokens":["becky","eat","gummy","bear","."]},{"$type":"ai.lum.odinson.TokensField","name":"entity","tokens":["I-PER","O","O","O","O"]},{"$type":"ai.lum.odinson.TokensField","name":"chunk","tokens":["B-NP","B-VP","B-NP","I-NP","O"]},{"$type":"ai.lum.odinson.GraphField","name":"dependencies","edges":[[1,0,"nsubj"],[1,3,"dobj"],[1,4,"punct"],[3,2,"amod"]],"roots":[1]}]}]}"""
 
   // extractor engine persists across tests (hacky way)
@@ -33,8 +30,8 @@ class TestRuleFile extends BaseSpec {
     testEventTrigger(m, start = 1, end = 2)
     // test arguments
     val desiredArgs = Seq(
-      Argument("subject", 0, 1),
-      Argument("object", 2, 4),
+      createArgument("subject", 0, 1),
+      createArgument("object", 2, 4),
     )
     testEventArguments(m, desiredArgs)
   }
