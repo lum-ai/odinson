f8e5d4bf097649c263d8e88bd75a016843b4b311
gcgbarbosa <gcgbarbosa@gmail.com>
2020-06-17 20:31:09 -0700

    Add todos

---
+++ core/src/test/scala/ai/lum/odinson/events/EventsSpec.scala
@@ -0,0 +1,8 @@
+package ai.lum.odinson.events
+
+import ai.lum.odinson.BaseSpec
+
+class EventSpec extends BaseSpec {
+  // TODO: move testEventTriger here
+  // TODO: move testEventArgument here
+}
