36064fd3bb4d1c1f86c9db6c6d750b339f337cba
bsharpataz <bsharp@email.arizona.edu>
2020-06-20 07:04:24 -0700

    Made the indexing a method to be called by outside projects

--- extra/src/main/scala/ai/lum/odinson/extra/IndexDocuments.scala
+++ extra/src/main/scala/ai/lum/odinson/extra/IndexDocuments.scala
@@ -8,6 +8,8 @@ import ai.lum.common.ConfigUtils._
 import ai.lum.common.FileUtils._
 import ai.lum.odinson.{ Document, Field, OdinsonIndexWriter, StringField }
 
+import scala.collection.GenIterable
+
 object IndexDocuments extends App with LazyLogging {
 
   val config = ConfigFactory.load()
@@ -34,30 +36,34 @@ object IndexDocuments extends App with LazyLogging {
     }
 
   logger.info("Indexing documents")
-
-  // index documents
-  for (f <- documentFiles) {
-    Try {
-      val origDoc = Document.fromJson(f)
-      // keep track of file name to retrieve sentence JSON,
-      // but ignore the path to the docs directory to avoid issues encountered when moving `odinson.dataDir`.
-      // NOTE: this assumes all files are located immediately under `odinson.docsDir`
-      // With large document collections, it may be necessary to split documents across many subdirectories
-      // To avoid peformance issues and limitations of certain file systems (ex. FAT32, ext2, etc.)
-      val fileField: Field = StringField(name = "fileName", string = f.getName, store = true)
-      val doc = origDoc.copy(metadata = origDoc.metadata ++ Seq(fileField))
-      val block = writer.mkDocumentBlock(doc)
-      writer.addDocuments(block)
-    } match {
-      case Success(_) =>
-        logger.info(s"Indexed ${f.getName}")
-      case Failure(e) =>
-        logger.error(s"Failed to index ${f.getName}", e)
-    }
-
-  }
+  indexDocuments(writer, documentFiles)
 
   writer.close
   // fin
+
+
+  def indexDocuments(writer: OdinsonIndexWriter, documentFiles: GenIterable[File]): Unit = {
+    // index documents
+    for (f <- documentFiles) {
+      Try {
+        val origDoc = Document.fromJson(f)
+        // keep track of file name to retrieve sentence JSON,
+        // but ignore the path to the docs directory to avoid issues encountered when moving `odinson.dataDir`.
+        // NOTE: this assumes all files are located immediately under `odinson.docsDir`
+        // With large document collections, it may be necessary to split documents across many subdirectories
+        // To avoid performance issues and limitations of certain file systems (ex. FAT32, ext2, etc.)
+        val fileField: Field = StringField(name = "fileName", string = f.getName, store = true)
+        val doc = origDoc.copy(metadata = origDoc.metadata ++ Seq(fileField))
+        val block = writer.mkDocumentBlock(doc)
+        writer.addDocuments(block)
+      } match {
+        case Success(_) =>
+          logger.info(s"Indexed ${f.getName}")
+        case Failure(e) =>
+          logger.error(s"Failed to index ${f.getName}", e)
+      }
+
+    }
+  }
 
 }
