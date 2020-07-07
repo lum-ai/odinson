e1d7923f6a274307f10170d0c7bbebcb7fe07847
gcgbarbosa <gcgbarbosa@gmail.com>
2020-07-06 17:49:44 -0700

    Polish TokensField test

--- core/src/test/scala/ai/lum/odinson/foundations/TestOdinsonDocument.scala
+++ core/src/test/scala/ai/lum/odinson/foundations/TestOdinsonDocument.scala
@@ -6,32 +6,41 @@ import ai.lum.odinson.{Document, BaseSpec}
 import ai.lum.odinson.{TokensField}
 
 class TestOdinsonDocument extends BaseSpec {
-  // TODO: case class Sentence
-  // TODO: test case class field
-  // TODO: test tokensField read
   "OdinsonDocument" should "handle a json String correctly" in {
     val field =
-      """{
-        "$type": "ai.lum.odinson.TokensField",
-        "name": "chunk",
-        "tokens": [
-          "B-NP",
-          "B-VP",
-          "B-NP",
-          "I-NP",
-          "O"
-        ]
-      }"""
+      """{"$type":"ai.lum.odinson.TokensField","name":"chunk","tokens":["B-NP","B-VP","B-NP","I-NP","O"]}"""
+    val fieldPretty = 
+      """|{
+        |    "$type": "ai.lum.odinson.TokensField",
+        |    "name": "chunk",
+        |    "tokens": [
+        |        "B-NP",
+        |        "B-VP",
+        |        "B-NP",
+        |        "I-NP",
+        |        "O"
+        |    ]
+        |}""".stripMargin
     //
     val tokenField = TokensField.fromJson(field)
     // check if the name is being parsed correct
-    tokenField.name shouldBe "chunk"
+    tokenField.name should be("chunk")
     // check if default is there
-    tokenField.store shouldBe false
-    // TODO: test individual tokens
+    tokenField.store should be(false)
+    // test toJson
+    tokenField.toJson should equal(field)
+    // test pretty
+    tokenField.toPrettyJson should equal(fieldPretty)
     // first
+    tokenField.tokens(0) should equal ("B-NP")
     // last
+    tokenField.tokens(4) should equal ("O")
   }
+  
+
+  // TODO: case class Sentence
+  // TODO: test case class field
+  // TODO: test tokensField read
   // TODO: test graphfield read
   // TODO: test stringfield read
   // TODO: datefield read
