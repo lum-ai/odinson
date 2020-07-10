a2bfd1ab3496551781cfaf8d93f42dc8ed8f7879
George C. G. Barbosa <gcgbarbosa@gmail.com>
2020-07-10 09:04:12 -0700

    Improve core tests - iteration 0 (#115)
    * Ignore scala formatter
    
    * Add tests and todos for ExtractorEngine
    
    * Add another todo
    
    * Add TestOdinsonDocument.scala + one test
    
    * Polish TokensField test
    
    * Add GraphField test
    
    * Remove todos already done
    
    * Add tests for StringField
    
    * Add tests for DateField
    
    * Polish unecessary comments
    
    * Add todos + add test to tJson and toPretty for Sentence
    
    * Add json files for testing
    
    * Fix indentation
    
    * Add tests for Document.fromJson([File])

---
+++ core/src/test/scala/ai/lum/odinson/foundations/TestExtractorEngine.scala
@@ -0,0 +1,62 @@
+package ai.lum.odinson.foundations
+
+import org.scalatest._
+
+import ai.lum.odinson.ExtractorEngine
+import ai.lum.odinson.BaseSpec
+import ai.lum.odinson.{TokensField, Sentence, Document}
+
+import org.apache.lucene.index.DirectoryReader
+import org.apache.lucene.document.{Document => LuceneDocument, _}
+import org.apache.lucene.document.Field.Store
+import com.typesafe.config.Config
+import ai.lum.common.ConfigFactory
+import ai.lum.common.ConfigUtils._
+import ai.lum.odinson.lucene._
+import ai.lum.odinson.lucene.search._
+import ai.lum.odinson.compiler.QueryCompiler
+import ai.lum.odinson.state.State
+
+class TestExtractorEngine extends BaseSpec {
+  val config = ConfigFactory.load()
+  val odinsonConfig = config[Config]("odinson")
+  val rawTokenField = config[String]("odinson.index.rawTokenField")
+  
+  // create a test sentence
+  val text = "Rain causes flood"
+  val tokens = TokensField(rawTokenField, text.split(" "), store = true)
+  val sentence = Sentence(tokens.tokens.length, Seq(tokens))
+  val doc1 = Document("<TEST-ID1>", Nil, Seq(sentence))
+  val doc2 = Document("<TEST-ID2>", Nil, Seq(sentence))
+  
+  val ee = ExtractorEngine.inMemory(Seq(doc1, doc2))
+  // TODO: the compiler should be tested first
+  // bc the rest of the stuff depends on it
+  // TODO: make this a fixture
+  // Check on code cov what to test
+  //
+  // test limited query
+  // figure out how to create an OdinsonQuery
+  // ee.query()
+  
+  "Odinson ExtractorEngine" should "run a simple query correctly" in {
+    val q = ee.compiler.mkQuery("causes")
+    val results = ee.query(q, 1)
+    results.totalHits should equal (2)
+  }
+
+  // TODO: implement index fixture to test the features bellow
+  // TODO: def getParentDoc(docId: String)
+  // TODO: def compileRules(rules: String)
+  // TODO: def extractMentions(extractors: Seq[Extractor], numSentences: Int)
+  // TODO: def query(odinsonQuery: OdinsonQuery, n: Int, after: OdinsonScoreDoc)
+  
+  // TODO: def getArgument(mention: Mention, name: String)
+  // TODO: getTokens(m: Mention): Array[String]
+  // TODO: getTokens(scoreDoc: OdinsonScoreDoc, fieldName: String): Array[String]
+  // TODO: ExtractorEngine.fromConfig
+  // TODO: ExtractorEngine.fromConfig(path: String)
+  // "Odinson ExtractorEngine" should "initialize correctly from config" in {
+  // }
+  // TODO: ExtractorEngine.fromConfig(config: Config)
+}
---
+++ core/src/test/scala/ai/lum/odinson/foundations/TestOdinsonDocument.scala
@@ -0,0 +1,182 @@
+package ai.lum.odinson.foundations
+
+import java.util.Date
+import java.text.SimpleDateFormat
+import java.io.File
+import scala.io.Source
+
+import org.scalatest._
+import ai.lum.odinson.{Document, BaseSpec}
+
+import ai.lum.odinson.{
+  TokensField,
+  GraphField,
+  Sentence,
+  Field,
+  StringField,
+  DateField
+}
+
+class TestOdinsonDocument extends BaseSpec {
+  "OdinsonDocument Document" should "handle a json File correctly" in {
+    // Check my code to see how to open a file like this
+    val jsonFile =
+      new File(getClass.getResource("/odinsonDocTest.json").getPath)
+    // Open the one life file
+    val docObj = Document.fromJson(jsonFile)
+    // check if toJson is working
+    docObj.toJson shouldEqual (Source
+      .fromResource("odinsonDocTest.json")
+      .getLines
+      .mkString)
+    // check if toPrettyJson is working
+    docObj.toPrettyJson shouldEqual (Source
+      .fromResource("odinsonDocTestPretty.json")
+      .getLines
+      .mkString("\n"))
+    // check if the fields are being loaded correctly
+    docObj.id shouldBe ("foo")
+    docObj.sentences.head.numTokens should be(1)
+    docObj.sentences.head.fields.head.name should be("raw")
+  }
+
+  "OdinsonDocument TokensField" should "handle a json String correctly" in {
+    val field =
+      """{"$type":"ai.lum.odinson.TokensField","name":"chunk","tokens":["B-NP","B-VP","B-NP","I-NP","O"]}"""
+    val fieldPretty =
+      """|{
+        |    "$type": "ai.lum.odinson.TokensField",
+        |    "name": "chunk",
+        |    "tokens": [
+        |        "B-NP",
+        |        "B-VP",
+        |        "B-NP",
+        |        "I-NP",
+        |        "O"
+        |    ]
+        |}""".stripMargin
+    //
+    val tokenField = TokensField.fromJson(field)
+    // check if the name is being parsed correct
+    tokenField.name should be("chunk")
+    // check if default is there
+    tokenField.store should be(false)
+    // test toJson
+    tokenField.toJson should equal(field)
+    // test pretty
+    tokenField.toPrettyJson should equal(fieldPretty)
+    // first
+    tokenField.tokens.head should equal("B-NP")
+    // last
+    tokenField.tokens.last should equal("O")
+  }
+
+  "OdinsonDocument GraphField" should "handle a json String correctly" in {
+    val field =
+      """{"$type":"ai.lum.odinson.GraphField","name":"dependencies","edges":[[1,0,"nsubj"],[3,2,"amod"]],"roots":[1]}"""
+    // parse json
+    val graphField = GraphField.fromJson(field)
+    // test name
+    graphField.name shouldBe ("dependencies")
+    // test roots
+    graphField.roots shouldBe a[Set[_]]
+    graphField.roots.head should equal(1)
+    // test store
+    graphField.store should be(false)
+    // test firs and last elements
+    graphField.edges.head shouldBe (1, 0, "nsubj")
+    graphField.edges.last shouldBe (3, 2, "amod")
+  }
+
+  // case class Sentence
+  "OdinsonDocument Sentence" should "handle a json String correctly" in {
+    val sentence =
+      """{"numTokens":1,"fields":[{"$type":"ai.lum.odinson.TokensField","name":"raw","tokens":["Becky"]},{"$type":"ai.lum.odinson.GraphField","name":"dependencies","edges":[[1,0,"nsubj"]],"roots":[1]}]}"""
+
+    // parse json
+    val sentenceObj = Sentence.fromJson(sentence)
+    // check the namber of tokens
+    sentenceObj.numTokens shouldEqual (1)
+    // make sure it gets the type of each field right
+    sentenceObj.fields.head shouldBe a[Field]
+    sentenceObj.fields.last shouldBe a[Field]
+    // check the type of both
+    val lastFieldType = sentenceObj.fields.last match {
+      case t: GraphField => "GraphField"
+      case _             => "other"
+    }
+    lastFieldType shouldBe ("GraphField")
+    // maybe test internals? ask
+    sentenceObj.toJson shouldEqual (sentence)
+    //
+    val prettySentence = """|{
+    |    "numTokens": 1,
+    |    "fields": [
+    |        {
+    |            "$type": "ai.lum.odinson.TokensField",
+    |            "name": "raw",
+    |            "tokens": [
+    |                "Becky"
+    |            ]
+    |        },
+    |        {
+    |            "$type": "ai.lum.odinson.GraphField",
+    |            "name": "dependencies",
+    |            "edges": [
+    |                [
+    |                    1,
+    |                    0,
+    |                    "nsubj"
+    |                ]
+    |            ],
+    |            "roots": [
+    |                1
+    |            ]
+    |        }
+    |    ]
+    |}""".stripMargin
+    // test pretty
+    sentenceObj.toPrettyJson shouldEqual (prettySentence)
+  }
+
+  "OdinsonDocument StringField" should "handle a json String correctly" in {
+    val field =
+      """{"$type":"ai.lum.odinson.StringField","name":"smth","string":"smthString"}"""
+    // parse field
+    val stringField = StringField.fromJson(field)
+    // check stuff
+    stringField.store shouldBe (false)
+    stringField.name shouldBe ("smth")
+    stringField.string shouldBe ("smthString")
+  }
+
+  "OdinsonDocument DateField" should "handle json String and local date correctly" in {
+    val field =
+      """{"$type":"ai.lum.odinson.DateField","name":"smth","date":"1993-03-28"}"""
+    // parse
+    var dateField = DateField.fromJson(field)
+    // test values
+    dateField.date shouldBe ("1993-03-28")
+    dateField.name shouldBe ("smth")
+    // test the parsed date
+    dateField.localDate.getYear shouldBe (1993)
+    dateField.localDate.getDayOfMonth shouldBe (28)
+    dateField.localDate.getMonthValue shouldBe (3)
+
+    val localDate = dateField.localDate
+    dateField = DateField.fromLocalDate("smth", localDate, false)
+    dateField.date shouldBe ("1993-03-28")
+    dateField.name shouldBe ("smth")
+  }
+
+  "OdinsonDocument DateField" should "handle java date correctly" in {
+    // testing java date
+    val formatter = new SimpleDateFormat("dd/MM/yyyy")
+    val javaDate = formatter.parse("28/03/1993")
+    val dateField = DateField.fromDate("smth", javaDate, false)
+    dateField.date shouldBe ("1993-03-28")
+    dateField.localDate.getYear shouldBe (1993)
+    dateField.localDate.getDayOfMonth shouldBe (28)
+    dateField.localDate.getMonthValue shouldBe (3)
+  }
+}
