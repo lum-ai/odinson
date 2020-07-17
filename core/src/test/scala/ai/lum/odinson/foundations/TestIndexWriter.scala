c21a007c00d7620778b7bad902d39c8a7d459079
gcgbarbosa <gcgbarbosa@gmail.com>
2020-07-17 08:22:57 -0700

    Add todos

---
+++ core/src/test/scala/ai/lum/odinson/foundations/TestIndexWriter.scala
@@ -0,0 +1,6 @@
+// TODO: test skiping sentencce in mkDocumentBlock
+// TODO: test mkMedatadaDoc(Document) lucenedoc.Document
+// TODO: test mkLucenefileds serialized dependencies too big for storage
+// TODO: test mkLuceneFields(Fields) Seq(Lucenedoc.Field)
+// TODO: test fromConfig
+
