4e158ce679248d20b8c68a8122a0c18197f7eb87
gcgbarbosa <gcgbarbosa@gmail.com>
2020-07-20 20:41:20 -0700

    Add more tests for graph traversals

--- core/src/test/scala/ai/lum/odinson/documentation/TestDocumentationGraphtraversals.scala
+++ core/src/test/scala/ai/lum/odinson/documentation/TestDocumentationGraphtraversals.scala
@@ -27,6 +27,7 @@ class TestDocumentationGraphTraversals extends EventSpec {
     )
     this.testEventArguments(s.scoreDocs.head.matches.head, desiredArgs)
   }
+ 
   "Documentation-GraphTraversals" should "work for '<foo' example" in {
     val ee = this.Utils.mkExtractorEngine(doc)
     // what is there should match
@@ -44,7 +45,7 @@ class TestDocumentationGraphTraversals extends EventSpec {
     )
     this.testEventArguments(s.scoreDocs.head.matches.head, desiredArgs)
   }
-  // TODO: test wildcards
+  
   "Documentation-GraphTraversals" should "work for '<<' example" in {
     val ee = this.Utils.mkExtractorEngine(doc)
     // what is there should match
@@ -62,6 +63,7 @@ class TestDocumentationGraphTraversals extends EventSpec {
     )
     this.testEventArguments(s.scoreDocs.head.matches.head, desiredArgs)
   }
+
   // make sure it matches the correct thing
   "Documentation-GraphTraversals" should "work for '>>' example" in {
     val ee = this.Utils.mkExtractorEngine(doc)
@@ -80,6 +82,30 @@ class TestDocumentationGraphTraversals extends EventSpec {
     )
     this.testEventArguments(s.scoreDocs.head.matches.head, desiredArgs)
   }
-  // TODO: test quantifiers
-  // TODO: test quantifiers
+  
+  "Documentation-GraphTraversals" should "work for '>>{2,3}' example" in {
+    val ee = this.Utils.mkExtractorEngine(doc)
+    // what is there should match
+    val pattern = """
+      trigger = [lemma=eat]
+      object: ^NP = >>{2,3}
+    """
+    val q = ee.compiler.compileEventQuery(pattern)
+    val s = ee.query(q)
+    // something that is not there should not match
+    s.totalHits shouldEqual (1)
+    this.testEventTrigger(s.scoreDocs.head.matches.head, start = 1, end = 2)
+    val desiredArgs = Seq(
+      createArgument("object", 2, 3)
+    )
+    this.testEventArguments(s.scoreDocs.head.matches.head, desiredArgs)
+    // this should not match
+    val pattern1 = """
+      trigger = [lemma=bear]
+      object: ^NP = >>{2,3}
+    """
+    val q1 = ee.compiler.compileEventQuery(pattern1)
+    val s1 = ee.query(q1)
+    s1.totalHits shouldEqual (0)
+  }
 } 
