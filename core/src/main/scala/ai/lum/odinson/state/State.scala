2e60ce6fb215160d69dee46f74a9b7b81d650e34
Keith Alcock <github@keithalcock.com>
2020-07-25 13:15:49 -0700

    Store events in State (#123)
    * Store events in State
    
    * Rename some mentions to resultItems

--- core/src/main/scala/ai/lum/odinson/state/State.scala
+++ core/src/main/scala/ai/lum/odinson/state/State.scala
@@ -2,11 +2,11 @@ package ai.lum.odinson.state
 
 trait State {
 
-  def addMentions(mentions: Iterator[(Int, Int, String, Int, Int)]): Unit
+  def addResultItems(resultItems: Iterator[ResultItem]): Unit
 
   def getDocIds(docBase: Int, label: String): Array[Int] // TODO: Return iterator
 
-  def getMatches(docBase: Int, docId: Int, label: String): Array[(Int, Int)] // TODO: Return iterator
+  def getResultItems(docBase: Int, docId: Int, label: String): Array[ResultItem] // TODO: Return iterator
 
   // This may eventually go away, but it is needed for testing just now.
 //  def close(): Unit = ()
