3531b5676297d7cf959fb78b127043181c44ede2
Robert Vacareanu <robertvacareanu@users.noreply.github.com>
2020-08-06 21:50:50 +0300

    Inject ExtractorEngine (#157)
    Inject ExtractorEngine in OdinsonController

---
+++ backend/app/utils/OdinsonProviderModule.scala
@@ -0,0 +1,17 @@
+package utils
+
+import javax.inject._
+
+import com.google.inject.AbstractModule
+import com.google.inject.Provides
+
+import ai.lum.odinson.ExtractorEngine
+
+class OdinsonProviderModule extends AbstractModule {
+  
+  override def configure() {}
+
+  @Provides @Singleton
+  def extractorEngineProvider(): ExtractorEngine = ExtractorEngine.fromConfig("odinson")
+  
+}
