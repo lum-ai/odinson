739eacc1abff7d046f5d91770c2953b10408d85c
bsharpataz <bsharp@email.arizona.edu>
2020-08-12 17:08:55 -0700

    added tests for quantifying traversals in parens

--- core/src/test/scala/ai/lum/odinson/traversals/TestTraversals.scala
+++ core/src/test/scala/ai/lum/odinson/traversals/TestTraversals.scala
@@ -7,9 +7,10 @@ class TestTraversals extends BaseSpec {
   val docAlien = getDocument("alien-species")
   val eeAlien = Utils.mkExtractorEngine(docAlien)
 
-  //val doc = Document.fromJson(json)
-  val doc = getDocumentFromJson(json)
-  val ee = Utils.mkExtractorEngine(doc)
+  // Some wild animals such as hedgehogs , coypu , and any wild cloven-footed animals
+  // such as deer and zoo animals including elephants can also contract it .
+  val docHedgehogs = getDocument("hedgehogs-coypy-2")
+  val eeHedgehogs = Utils.mkExtractorEngine(docHedgehogs)
 
   "Odinson" should "find all matches across conj_and" in {
     val pattern = "[word=cats] >conj_and [tag=/N.*/]"
@@ -33,6 +34,55 @@ class TestTraversals extends BaseSpec {
     val Array(m1, m2) = results.scoreDocs.head.matches
     eeAlien.getString(doc, m1) should equal ("horses")
     eeAlien.getString(doc, m2) should equal ("cattle")
+  }
+
+  // A little helper method to reduce code duplication for the following tests
+  def testHedgehogQuantifier(quantifier: String, expectedMatches: Array[String]) = {
+    val pattern = s"[word=animals] (>nmod_such_as [])${quantifier}"
+    val query = eeHedgehogs.compiler.mkQuery(pattern)
+    val results = eeHedgehogs.query(query, 1)
+    results.totalHits should equal (1)
+    val matches = results.scoreDocs.head.matches
+//    matches should have size expectedMatches.length
+    val doc = results.scoreDocs.head.doc
+    val foundStrings = matches.map(m => eeHedgehogs.getString(doc, m))
+    foundStrings shouldEqual expectedMatches
+  }
+
+  // FIXME: if lazy, we expect ["animals", "animals", "animals"]
+  it should "support quantifiers on groups of graph traversals and surface patterns -- optional" in {
+    testHedgehogQuantifier("?", Array("hedgehogs", "coypu", "yyymals"))
+  }
+
+  it should "support quantifiers on groups of graph traversals and surface patterns -- ranges {1}" in {
+    testHedgehogQuantifier("{1}", Array("hedgehogs", "coypu", "yyymals"))
+  }
+
+  it should "support quantifiers on groups of graph traversals and surface patterns -- ranges {2}" in {
+    testHedgehogQuantifier("{2}", Array("deer", "zzzmals"))
+  }
+
+  // FIXME: I think I expect it to return all
+  it should "support quantifiers on groups of graph traversals and surface patterns -- ranges {1,2}" in {
+    testHedgehogQuantifier("{1,2}", Array("hedgehogs", "coypu", "yyymals", "deer", "zzzmals"))
+  }
+
+  // FIXME: I think I expect it to return all
+  it should "support quantifiers on groups of graph traversals and surface patterns -- kleene plus" in {
+    testHedgehogQuantifier("+", Array("hedgehogs", "coypu", "yyymals", "deer", "zzzmals"))
+  }
+
+  // FIXME: I think I expect it to return all, including with 0 of the traversals (i.e., "animals")
+  it should "support quantifiers on groups of graph traversals and surface patterns -- kleene star" in {
+    testHedgehogQuantifier("*", Array("animals", "hedgehogs", "coypu", "yyymals", "deer", "zzzmals"))
+  }
+
+  // FIXME: this doesn't compile but should
+  it should "support half steps inside the parentheses" in {
+    val pattern = "[word=animals] (>nmod_such_as)?"
+    val query = eeHedgehogs.compiler.mkQuery(pattern)
+    val results = eeHedgehogs.query(query, 1)
+    results.totalHits should equal (1)
   }
 
 }
