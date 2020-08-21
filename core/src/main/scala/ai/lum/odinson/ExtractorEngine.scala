b27cf05e6da7ef67be7e05c329ce5a40cf7b065c
Keith Alcock <github@keithalcock.com>
2020-08-20 20:42:13 -0700

    Fix merge problem

--- core/src/main/scala/ai/lum/odinson/ExtractorEngine.scala
+++ core/src/main/scala/ai/lum/odinson/ExtractorEngine.scala
@@ -37,21 +37,6 @@ object LazyIdGetter {
   def apply(indexSearcher: OdinsonIndexSearcher, docId: Int): LazyIdGetter = new LazyIdGetter(indexSearcher, docId)
 }
 
-
-class LazyIdGetter(indexSearcher: OdinsonIndexSearcher, documentId: Int) extends IdGetter {
-  protected lazy val document = indexSearcher.doc(documentId)
-  protected lazy val docId: String = document.getField("docId").stringValue
-  protected lazy val sentId: String = document.getField("sentId").stringValue
-
-  def getDocId: String = docId
-
-  def getSentId: String = sentId
-}
-
-object LazyIdGetter {
-  def apply(indexSearcher: OdinsonIndexSearcher, docId: Int): LazyIdGetter = new LazyIdGetter(indexSearcher, docId)
-}
-
 class ExtractorEngine(
   val indexSearcher: OdinsonIndexSearcher,
   val compiler: QueryCompiler,
