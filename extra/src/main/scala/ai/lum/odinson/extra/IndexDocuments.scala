55c02c8ec3c2ec0c8fc43d42dca72a79af3a7f7e
George C. G. Barbosa <gcgbarbosa@gmail.com>
2020-08-24 13:31:09 -0700

    Add a test for IndexDocuments in `extra` (#170)
    * Start the branch with tests for `extra`
    
    * Add json documents
    
    * Add option to receive the path as arguments
    
    * Add a test for IndexDocuments
    
    * Help double test/run avoid DLL conflicts
    
    * Compressed one file used by `extra` tests
    
    * validating docs before indexing and made test resources smaller (#173)
    
    * typo
    
    * failing test
    
    * update proc version
    
    * doc with bad character
    
    * test for invalid control character
    
    * OdinsonIndexWriter now checks TokenFields for invalid tokens
    
    and replaces them with a specified token, if found.
    
    * BioCluProcessor is no longer in main CluProcessor package
    
    also, updated the test files, so now the odinson docs
    files are smaller.
    
    * revert processors to 7.4.4
    
    Co-authored-by: Keith Alcock <github@keithalcock.com>
    Co-authored-by: Becky Sharp <bsharp@email.arizona.edu>

--- extra/src/main/scala/ai/lum/odinson/extra/IndexDocuments.scala
+++ extra/src/main/scala/ai/lum/odinson/extra/IndexDocuments.scala
@@ -1,25 +1,46 @@
 package ai.lum.odinson.extra
 
 import java.io._
-import scala.util.{ Try, Success, Failure }
+import scala.util.{Try, Success, Failure}
 import com.typesafe.scalalogging.LazyLogging
+
 import ai.lum.common.ConfigFactory
+import com.typesafe.config.{Config, ConfigValueFactory}
+
 import ai.lum.common.ConfigUtils._
 import ai.lum.common.FileUtils._
-import ai.lum.odinson.{ Document, Field, OdinsonIndexWriter, StringField }
+import ai.lum.odinson.{Document, Field, OdinsonIndexWriter, StringField}
 
 import scala.collection.GenIterable
 
 object IndexDocuments extends App with LazyLogging {
-
-  val config = ConfigFactory.load()
+  //
+  var config = ConfigFactory.load()
+  // inject a new arg for the folder
+  if (args.length == 1) {
+    logger.info(s"Received dataDir as a parameter <${args(0)}>")
+    // receive the path from the arguments
+    config = config
+      .withValue("odinson.dataDir", ConfigValueFactory.fromAnyRef(args(0)))
+      // re-compute the index and docs path's
+      .withValue(
+        "odinson.indexDir",
+        ConfigValueFactory.fromAnyRef(args(0) + "/index")
+      )
+      .withValue(
+        "odinson.docsDir",
+        ConfigValueFactory.fromAnyRef(args(0) + "/docs")
+      )
+  }
+  //
   val docsDir = config[File]("odinson.docsDir")
-  val synchronizeOrderWithDocumentId = config[Boolean]("odinson.index.synchronizeOrderWithDocumentId")
-  val writer = OdinsonIndexWriter.fromConfig()
+  val synchronizeOrderWithDocumentId =
+    config[Boolean]("odinson.index.synchronizeOrderWithDocumentId")
+  //
+  val writer = OdinsonIndexWriter.fromConfig(config.getConfig("odinson"))
   val wildcards = Seq("*.json", "*.json.gz")
-
   logger.info(s"Gathering documents from $docsDir")
-
+  // make this a function
   val documentFiles =
     if (synchronizeOrderWithDocumentId) {
       // files ordered by the id of the document
@@ -34,16 +55,16 @@ object IndexDocuments extends App with LazyLogging {
         .listFilesByWildcards(wildcards, recursive = true)
         .par
     }
-
+  // ^ this part should be a function
   logger.info("Indexing documents")
   indexDocuments(writer, documentFiles)
-
   writer.close
   // fin
-
-
   // Note that documentFiles may or may not be parallel, hence the GenIterable
-  def indexDocuments(writer: OdinsonIndexWriter, documentFiles: GenIterable[File]): Unit = {
+  def indexDocuments(
+      writer: OdinsonIndexWriter,
+      documentFiles: GenIterable[File]
+  ): Unit = {
     // index documents
     for (f <- documentFiles) {
       Try {
@@ -53,7 +74,8 @@ object IndexDocuments extends App with LazyLogging {
         // NOTE: this assumes all files are located immediately under `odinson.docsDir`
         // With large document collections, it may be necessary to split documents across many subdirectories
         // To avoid performance issues and limitations of certain file systems (ex. FAT32, ext2, etc.)
-        val fileField: Field = StringField(name = "fileName", string = f.getName, store = true)
+        val fileField: Field =
+          StringField(name = "fileName", string = f.getName, store = true)
         val doc = origDoc.copy(metadata = origDoc.metadata ++ Seq(fileField))
         val block = writer.mkDocumentBlock(doc)
         writer.addDocuments(block)
@@ -63,8 +85,6 @@ object IndexDocuments extends App with LazyLogging {
         case Failure(e) =>
           logger.error(s"Failed to index ${f.getName}", e)
       }
-
     }
   }
-
 }
---
+++ extra/src/test/scala/ai/lum/odinson/extra/TestIndexDocuments.scala
@@ -0,0 +1,48 @@
+package ai.lum.odinson.extra
+
+import org.scalatest._
+
+import ai.lum.common.ConfigFactory
+import com.typesafe.config.{Config, ConfigValueFactory}
+
+import ai.lum.odinson.ExtractorEngine
+
+import scala.reflect.io.Directory
+import java.io.File
+
+// TODO: can I also extend basespec from here?
+class TestIndexDocuments extends FlatSpec with Matchers {
+  // get the resources folder
+  val resourcesFolder = getClass.getResource("/").getFile
+  // 
+  def deleteIndex = {
+    val dir = new Directory(new File(resourcesFolder + "index"))
+    dir.deleteRecursively()
+  }
+  // make sure the function that reads the files work when pointing to the resources
+  "IndexDocuments" should "get the correct list of files" in {
+    // delete index if it already exists
+    deleteIndex
+    // run stuff
+    IndexDocuments.main(Array(resourcesFolder))
+    // get config and ingect required values
+    var config = ConfigFactory.load()
+    config = config
+      .withValue("odinson.dataDir", ConfigValueFactory.fromAnyRef(resourcesFolder))
+      // re-compute the index and docs path's
+      .withValue(
+        "odinson.indexDir",
+        ConfigValueFactory.fromAnyRef(resourcesFolder + "/index")
+      )
+      .withValue(
+        "odinson.docsDir",
+        ConfigValueFactory.fromAnyRef(resourcesFolder + "/docs")
+      )
+    // get an ee
+    val ee = ExtractorEngine.fromConfig(config.getConfig("odinson"))
+    // make sure the files are there
+    // There are two files, one with 150 sentences + 1 parent doc, and one
+    // with 100 sentences + 1 parent doc = 252 docs
+    ee.numDocs shouldEqual (252)
+  }
+}
