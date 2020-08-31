8d761cd4706d3e1bbf6419c01b75c181088bf924
bsharpataz <bsharp@email.arizona.edu>
2020-08-30 21:16:49 -0700

    tests for imports

--- core/src/test/scala/ai/lum/odinson/events/TestRuleFile.scala
+++ core/src/test/scala/ai/lum/odinson/events/TestRuleFile.scala
@@ -1,9 +1,17 @@
 package ai.lum.odinson.events
 
+import ai.lum.common.FileUtils._
+import java.io.File
+import java.nio.file.{Files, Path}
+
 class TestRuleFile extends EventSpec {
   // extractor engine persists across tests (hacky way)
-  def doc = getDocument("becky-gummy-bears-v2")
-  def ee = Utils.mkExtractorEngine(doc)
+  def docGummy = getDocument("becky-gummy-bears-v2")
+  def eeGummy = Utils.mkExtractorEngine(docGummy)
+
+  // Leonardo leads, Donatello does machines (That's a fact, jack!)
+  val docNinja = getDocument("ninja-turtles")
+  val eeNinja = Utils.mkExtractorEngine(docNinja)
 
   "Odinson" should "match event with rules defined in a rule file" in {
     val rules = """
@@ -18,8 +26,8 @@ class TestRuleFile extends EventSpec {
       |      subject: ^NP = >nsubj ${chunk}
       |      object: ^NP = >dobj ${chunk}
     """.stripMargin
-    val extractors = ee.ruleReader.compileRuleString(rules)
-    val mentions = ee.extractMentions(extractors)
+    val extractors = eeGummy.ruleReader.compileRuleString(rules)
+    val mentions = eeGummy.extractMentions(extractors)
     mentions should have size 1
     val m = mentions.head.odinsonMatch
     // test trigger
@@ -33,9 +41,6 @@ class TestRuleFile extends EventSpec {
   }
 
   it should "correctly handle list vars" in {
-    // Leonardo leads, Donatello does machines (That's a fact, jack!)
-    val doc = getDocument("ninja-turtles")
-    val ee = Utils.mkExtractorEngine(doc)
     val rules =
       """
         |vars:
@@ -52,15 +57,15 @@ class TestRuleFile extends EventSpec {
         |    pattern: |
         |      [norm=/${turtle}/]
        """.stripMargin
-    val extractors = ee.compileRuleString(rules)
-    val mentions = ee.extractMentions(extractors)
+    val extractors = eeNinja.compileRuleString(rules)
+    val mentions = eeNinja.extractMentions(extractors)
     mentions should have size 2
   }
 
   it should "allow rules to be imported in a master rule file" in {
     val masterPath = "/testGrammar/testMaster.yml"
-    val extractors = ee.compileRuleResource(masterPath)
-    val mentions = ee.extractMentions(extractors)
+    val extractors = eeGummy.compileRuleResource(masterPath)
+    val mentions = eeGummy.extractMentions(extractors)
     mentions should have size 1
     // Tests that the variables from the master file propagate
     mentions.head.foundBy should be("testRuleImported-IMPORT_LABEL")
@@ -68,9 +73,112 @@ class TestRuleFile extends EventSpec {
 
   // todo:
   //  1: import in a string should throw an exception
-  //  2: a string w/o import should not throw an exception
+  it should "throw an exception with imports in string" in {
+    // Leonardo leads, Donatello does machines (That's a fact, jack!)
+    val doc = getDocument("ninja-turtles")
+    val ee = Utils.mkExtractorEngine(doc)
+    val rules =
+      """
+        |vars:
+        |  turtle:
+        |     - leonardo
+        |     - donatello
+        |     - raphael
+        |     - michelangelo
+        |
+        |rules:
+        |  - import: /testGrammar/testRules.yml
+        |
+       """.stripMargin
+    assertThrows[AssertionError]{
+      ee.compileRuleString(rules)
+    }
+  }
+  //  2: a string w/o import should not throw an exception -- see above
+
   //  3: test resources with absolute and relative paths (with .. notation)
-  //  4: test filesystem resources -- write temp directory with files, put a grammar there, delete when done (if possible)
   //  5: test that the hard-coded > import > parent > local
+  it should "allow resource file imports with absolute and relative paths and handle variables" in {
+    val masterPath = "/testGrammar/testPaths.yml"
+    val extractors = eeNinja.compileRuleResource(masterPath, Map("otherName" -> "HARD_CODED"))
+    val mentions = eeNinja.extractMentions(extractors)
+    mentions should have size 3
+    val leadsMentions = mentions.filter(m => eeNinja.getString(m.luceneDocId, m.odinsonMatch) == "leads")
+    assert(leadsMentions.length == 1)
+    // because import beats both parent (in a.yml) and local (in b.yml)
+    leadsMentions.head.foundBy should be("B-IMPORT_FROM_A")
+
+    val machinesMentions = mentions.filter(m => eeNinja.getString(m.luceneDocId, m.odinsonMatch) == "machines")
+    assert(machinesMentions.length == 1)
+    // because import beats both parent and local
+    machinesMentions.head.foundBy should be("A-IMPORT_NAME")
+
+    val factMentions = mentions.filter(m => eeNinja.getString(m.luceneDocId, m.odinsonMatch) == "fact")
+    assert(factMentions.length == 1)
+    // no import, but parent beats local, and hard-coded trumps all
+    factMentions.head.foundBy should be("C-C-PARENT-HARD_CODED")
+  }
+
+  //  4: test filesystem resources -- write temp directory with files, put a grammar there, delete when done (if possible)
+  it should "allow for absolute and relative paths in filesystem" in {
+    val tempDir = Files.createDirectory(new File("tmp-grammar").toPath).toFile
+    tempDir.deleteOnExit()
+
+    val masterFile = new File(tempDir, "tmpMaster.yml")
+    masterFile.deleteOnExit()
+
+    val importDir = Files.createDirectory(new File("tmp-grammar", "imported").toPath).toFile
+    importDir.deleteOnExit()
+
+    val aFile = new File(importDir, "a.yml")
+    aFile.deleteOnExit()
+
+    val bFile = new File(importDir, "b.yml")
+    bFile.deleteOnExit()
+
+    val bRelative = new File("imported", "b.yml")
+
+    val masterContents =
+      s"""
+        |vars:
+        |  chunk: "[chunk=B-NP][chunk=I-NP]*"
+        |
+        |rules:
+        |  - import: ${aFile.getAbsolutePath}
+        |
+        |  - import: ${bRelative}
+        """.stripMargin
+
+    val aContents =
+      """
+        |rules:
+        |  - name: A
+        |    type: basic
+        |    pattern: |
+        |      machines
+        """.stripMargin
+
+    val bContents =
+      """
+        |rules:
+        |  - name: B
+        |    type: basic
+        |    pattern: |
+        |      leads
+        """.stripMargin
+
+    masterFile.writeString(masterContents)
+    aFile.writeString(aContents)
+    bFile.writeString(bContents)
+
+    val extractors = eeNinja.compileRuleFile(masterFile)
+    val mentions = eeNinja.extractMentions(extractors)
+    mentions should have size 2
+    val leadsMentions = mentions.filter(m => eeNinja.getString(m.luceneDocId, m.odinsonMatch) == "leads")
+    assert(leadsMentions.length == 1)
+
+    val machinesMentions = mentions.filter(m => eeNinja.getString(m.luceneDocId, m.odinsonMatch) == "machines")
+    assert(machinesMentions.length == 1)
+  }
 
 }
