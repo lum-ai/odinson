f66c7ccafb4ccaf00c1960fbb19faf310325e54f
Keith Alcock <github@keithalcock.com>
2020-09-07 10:32:37 -0700

    Move some code to companion object

--- core/src/main/scala/ai/lum/odinson/state/MemoryState.scala
+++ core/src/main/scala/ai/lum/odinson/state/MemoryState.scala
@@ -8,25 +8,9 @@ import scala.collection.mutable
 // serialize the OdinsonMatches and then deserialize them as StateMatches.  This version keeps
 // the matches just as they are.  This might cause behavior changes in clients.  Beware!
 class MemoryState extends State {
-  case class BaseIdLabel(docBase: Int, docId: Int, label: String)
-  case class BaseLabel(docBase: Int, label: String)
+  import MemoryState._
 
-  // This original implementation is thought to create too many objects.
-  // implicit val ordering: Ordering[ResultItem] = Ordering.by[ResultItem, (Int, Int)] { resultItem =>
-  //   (resultItem.odinsonMatch.start, resultItem.odinsonMatch.end)
-  // }
-
-  implicit object ResultItemOrdering extends Ordering[ResultItem] {
-    def compare(left: ResultItem, right: ResultItem): Int = {
-      val startSign = left.odinsonMatch.start - right.odinsonMatch.start
-      if (startSign != 0) startSign
-      else {
-        val endSign = left.odinsonMatch.end - right.odinsonMatch.end
-        endSign // and if these are tied...
-      }
-    }
-  }
-
+  implicit val resultItemOrdering = MemoryState.ResultItemOrdering
   protected val baseIdLabelToResultItems: mutable.Map[BaseIdLabel, mutable.SortedSet[ResultItem]] = mutable.Map.empty
   protected val baseLabelToIds: mutable.Map[BaseLabel, mutable.SortedSet[Int]] = mutable.Map.empty
 
@@ -60,6 +44,28 @@ class MemoryState extends State {
     val resultItems = resultItemsOpt.map(_.toArray).getOrElse(Array.empty)
 
     resultItems
+  }
+}
+
+object MemoryState {
+  case class BaseIdLabel(docBase: Int, docId: Int, label: String)
+  case class BaseLabel(docBase: Int, label: String)
+
+  // This original implementation is thought to create too many objects.
+  // implicit val ordering: Ordering[ResultItem] = Ordering.by[ResultItem, (Int, Int)] { resultItem =>
+  //   (resultItem.odinsonMatch.start, resultItem.odinsonMatch.end)
+  // }
+
+  // The compilare can't handle an implicit here.
+  object ResultItemOrdering extends Ordering[ResultItem] {
+    def compare(left: ResultItem, right: ResultItem): Int = {
+      val startSign = left.odinsonMatch.start - right.odinsonMatch.start
+      if (startSign != 0) startSign
+      else {
+        val endSign = left.odinsonMatch.end - right.odinsonMatch.end
+        endSign // and if these are tied...
+      }
+    }
   }
 }
 
