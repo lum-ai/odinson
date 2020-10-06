bca194fcdec6495f77c9c8d3fa5191ab3af96446
Keith Alcock <github@keithalcock.com>
2020-10-05 22:34:38 -0700

    Remove unused class

--- core/src/main/scala/ai/lum/odinson/mention/SuperMentionIterator.scala
+++
@@ -1,9 +0,0 @@
-package ai.lum.odinson.mention
-
-class SuperMentionIterator(iterator: MentionIterator) extends MentionIterator {
-  override def close(): Unit = iterator.close()
-
-  override def hasNext: Boolean = iterator.hasNext
-
-  override def next(): Mention = iterator.next()
-}
