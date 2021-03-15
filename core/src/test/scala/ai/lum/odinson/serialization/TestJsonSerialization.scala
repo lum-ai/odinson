30310587f73f9dcfdbf3cf8018441d46f28ea2c7
bsharpataz <bsharp@email.arizona.edu>
2021-03-15 00:40:42 -0700

    added tests for verbosity

--- core/src/test/scala/ai/lum/odinson/serialization/TestJsonSerialization.scala
+++ core/src/test/scala/ai/lum/odinson/serialization/TestJsonSerialization.scala
@@ -1,21 +1,33 @@
 package ai.lum.odinson.serialization
 
+import java.util
+
 import ai.lum.odinson.utils.TestUtils.OdinsonTest
+import ai.lum.odinson.utils.exceptions.OdinsonException
+import com.typesafe.config.ConfigValueFactory
 
 class TestJsonSerialization extends OdinsonTest {
 
   val doc = getDocument("rainbows")
   val engine = mkExtractorEngine(doc)
+  val storedFields = util.Arrays.asList("raw", "lemma", "tag")
+  val verboseEngine = mkExtractorEngine(
+    defaultConfig.withValue("odinson.index.storedFields", ConfigValueFactory.fromIterable(storedFields)),
+    doc
+  )
 
   val extractors = engine.compileRuleResource("/serialization.yml")
 
   // Without state
   val mentions = engine.extractNoState(extractors).toArray
+  val jsonSerializer = new JsonSerializer(verbose = JsonSerializer.NONE, indent = 4, engine = Some(engine))
+  val displaySerializer = new JsonSerializer(verbose = JsonSerializer.DISPLAY, indent = 4, engine = Some(verboseEngine))
+  val allSerializer = new JsonSerializer(verbose = JsonSerializer.ALL, indent = 4, engine = Some(verboseEngine))
 
   "JsonSerializer" should "handle NGramMentions" in {
     val m = getSingleMentionFromRule(mentions, "NGram")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -23,8 +35,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle basic EventMatches" in {
     val m = getSingleMentionFromRule(mentions, "Event")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -32,8 +44,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle EventMatches with arg quantifiers" in {
     val m = getSingleMentionFromRule(mentions, "Event-plus")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -41,8 +53,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle EventMatches with arg ranges" in {
     val m = getSingleMentionFromRule(mentions, "Event-3")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -50,8 +62,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle GraphTraversals" in {
     val m = getSingleMentionFromRule(mentions, "GraphTraversal")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -59,8 +71,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle Repetition" in {
     val m = getSingleMentionFromRule(mentions, "Repetition")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -68,8 +80,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle Repetition (lazy)" in {
     val m = getSingleMentionFromRule(mentions, "Repetition-lazy")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -77,8 +89,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle Optional" in {
     val m = getSingleMentionFromRule(mentions, "Optional")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -86,8 +98,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle Or" in {
     val m = getSingleMentionFromRule(mentions, "Or")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -95,8 +107,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle Named" in {
     val m = getSingleMentionFromRule(mentions, "Named")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -107,8 +119,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   "JsonSerializer" should "handle NGramMentions with State" in {
     val m = getSingleMentionFromRule(stateMentions, "NGram")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -116,8 +128,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle basic EventMatches with State" in {
     val m = getSingleMentionFromRule(stateMentions, "Event")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -125,8 +137,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle EventMatches with arg quantifiers with State" in {
     val m = getSingleMentionFromRule(stateMentions, "Event-plus")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -134,8 +146,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle EventMatches with arg ranges with State" in {
     val m = getSingleMentionFromRule(stateMentions, "Event-3")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -143,8 +155,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle GraphTraversals with State" in {
     val m = getSingleMentionFromRule(stateMentions, "GraphTraversal")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -152,8 +164,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle Repetition with State" in {
     val m = getSingleMentionFromRule(stateMentions, "Repetition")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -161,8 +173,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle Repetition (lazy) with State" in {
     val m = getSingleMentionFromRule(stateMentions, "Repetition-lazy")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -170,8 +182,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle Optional with State" in {
     val m = getSingleMentionFromRule(stateMentions, "Optional")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -179,8 +191,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle Or with State" in {
     val m = getSingleMentionFromRule(stateMentions, "Or")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -188,8 +200,8 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "handle Named with State" in {
     val m = getSingleMentionFromRule(stateMentions, "Named")
-    val json = JsonSerializer.asJsonValue(Array(m))
-    val reconstituted = JsonSerializer.deserializeMentions(json)
+    val json = jsonSerializer.asJsonValue(Array(m))
+    val reconstituted = jsonSerializer.deserializeMentions(json)
     reconstituted should have length (1)
 
     mentionsShouldBeEqual(m, reconstituted.head) should be(true)
@@ -197,23 +209,45 @@ class TestJsonSerialization extends OdinsonTest {
 
   it should "properly serialize and deserialized using json strings" in {
     val m = getSingleMentionFromRule(stateMentions, "Or")
-    val jsonString = JsonSerializer.asJsonString(m)
-    val deserialized = JsonSerializer.deserializeMention(jsonString)
+    val jsonString = jsonSerializer.asJsonString(m)
+    val deserialized = jsonSerializer.deserializeMention(jsonString)
     mentionsShouldBeEqual(m, deserialized) should be(true)
 
-    val jsonPretty = JsonSerializer.asJsonPretty(m)
-    val deserializedPretty = JsonSerializer.deserializeMention(jsonPretty)
+    val jsonPretty = jsonSerializer.asJsonPretty(m)
+    val deserializedPretty = jsonSerializer.deserializeMention(jsonPretty)
     mentionsShouldBeEqual(m, deserializedPretty) should be(true)
   }
 
   it should "properly serialize and deserialized using json lines" in {
     val m1 = getSingleMentionFromRule(mentions, "Or")
     val m2 = getSingleMentionFromRule(mentions, "Named")
-    val jsonLines = JsonSerializer.asJsonLines(Seq(m1, m2))
-    val deserialized = JsonSerializer.deserializeJsonLines(jsonLines)
+    val jsonLines = jsonSerializer.asJsonLines(Seq(m1, m2))
+    val deserialized = jsonSerializer.deserializeJsonLines(jsonLines)
     deserialized should have length (2)
     mentionsShouldBeEqual(m1, deserialized(0)) should be(true)
     mentionsShouldBeEqual(m2, deserialized(1)) should be(true)
+  }
+
+  "JsonSerializer with verbose=display" should "include the display field and content" in {
+    val nonevent = getSingleMentionFromRule(mentions, "MultipleWords")
+    val json = displaySerializer.asJsonValue(nonevent)
+    val detail = json("detail")
+    val raw = detail("raw").arr.map(_.str)
+    raw should contain inOrderOnly ("Rainbows", "shine", "bright")
+
+    a [java.util.NoSuchElementException] should be thrownBy json("lemma")
+  }
+
+  "JsonSerializer with verbose=all" should "include the all stored fields and content" in {
+    val nonevent = getSingleMentionFromRule(mentions, "MultipleWords")
+    val json = allSerializer.asJsonValue(nonevent)
+    val detail = json("detail")
+
+    detail("raw").arr.map(_.str) should contain inOrderOnly ("Rainbows", "shine", "bright")
+    detail("lemma").arr.map(_.str) should contain inOrderOnly ("rainbow", "shine", "bright")
+    detail("tag").arr.map(_.str) should contain inOrderOnly ("NNS", "VBP", "JJ")
+
+    a [java.util.NoSuchElementException] should be thrownBy json("watermelon")
   }
 
 }
