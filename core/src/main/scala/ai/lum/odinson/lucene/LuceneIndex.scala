797456eb9823be62091d6d5b1f4ea13cba4f44f1
Michael Reynolds <michael.reynolds@twosixlabs.com>
2021-05-18 14:29:17 -0400

    add a POC of a lucene index manager class that does incremental index updates

---
+++ core/src/main/scala/ai/lum/odinson/lucene/LuceneIndex.scala
@@ -0,0 +1,78 @@
+package ai.lum.odinson.lucene
+
+import org.apache.lucene.analysis.Analyzer
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer
+import org.apache.lucene.document.Document
+import org.apache.lucene.index.IndexWriterConfig.OpenMode
+import org.apache.lucene.index.{IndexWriter, IndexWriterConfig}
+import org.apache.lucene.search.{IndexSearcher, Query, SearcherManager, TopDocs}
+import org.apache.lucene.store.Directory
+
+import java.util.Collection
+import scala.collection.JavaConverters._
+import scala.concurrent.{ExecutionContext, Future}
+
+
+trait LuceneIndex {
+
+    protected val analyzer : Analyzer = new WhitespaceAnalyzer
+
+    protected val directory : Directory
+
+    def write( block : Collection[ Document ] ) : Unit
+
+    def search( query : Query, limit : Int = 1000000000 ) : TopDocs
+
+    def refresh( ) : Unit
+
+}
+
+class IncrementalLuceneIndex( val directory : Directory, refreshMs : Long = 1000 ) extends LuceneIndex {
+
+    private implicit val ec : ExecutionContext = ExecutionContext.global
+
+    private val writer : IndexWriter = {
+        val config = new IndexWriterConfig( this.analyzer )
+        config.setOpenMode( OpenMode.CREATE_OR_APPEND )
+        new IndexWriter( this.directory, config )
+    }
+
+    private val manager : SearcherManager = new SearcherManager( writer, null )
+
+    refreshPeriodically()
+
+    override def search( query : Query, limit : Int ) : TopDocs = {
+        var searcher : IndexSearcher = null
+        try {
+            searcher = acquireSearcher()
+            searcher.search( query, limit )
+        } catch {
+            case e : Throwable => throw new RuntimeException( "what is the best way to deal with this?" )
+        }
+        finally releaseSearcher( searcher )
+    }
+
+    override def write( block : Collection[ Document ] ) : Unit = {
+        println( s"adding document ${block.asScala.foreach( println )}" )
+        writer.addDocuments( block )
+        refresh()
+    }
+
+    override def refresh( ) : Unit = {
+        writer.flush()
+        writer.commit()
+        manager.maybeRefresh()
+    }
+
+    private def acquireSearcher( ) : IndexSearcher = manager.acquire()
+
+    private def releaseSearcher( searcher : IndexSearcher ) : Unit = manager.release( searcher )
+
+    private def refreshPeriodically( ) : Unit = {
+        Future {
+            println( "refreshing index searchers with updated data" )
+            Thread.sleep( refreshMs )
+            refresh()
+        }
+    }
+}
---
+++ core/src/test/scala/ai/lum/odinson/lucene/TestLuceneIndex.scala
@@ -0,0 +1,68 @@
+package ai.lum.odinson.lucene
+
+
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer
+import org.apache.lucene.document.{Document, Field, TextField}
+import org.apache.lucene.queryparser.classic.QueryParser
+import org.apache.lucene.search.Query
+import org.apache.lucene.store.FSDirectory
+import org.scalatest.{BeforeAndAfterEach, FlatSpecLike, Matchers}
+
+import java.io.File
+import scala.collection.JavaConverters._
+import scala.reflect.io.Directory
+
+class TestLuceneIndex extends FlatSpecLike with Matchers with BeforeAndAfterEach {
+
+    private val indexDir : File = new File( "./target/lucene-indexer-test" )
+
+    override def beforeEach( ) : Unit = {
+        if ( indexDir.exists() ) new Directory( indexDir ).deleteRecursively()
+    }
+
+    override def afterEach( ) : Unit = {
+        if ( indexDir.exists() ) new Directory( indexDir ).deleteRecursively()
+    }
+
+    "Incremental Lucene Index" should "add a document" in {
+        val index : LuceneIndex = new IncrementalLuceneIndex( FSDirectory.open( indexDir.toPath ), 100 )
+
+        // lucene field mappings
+        val docIdField = "id"
+        val fieldName = "text_content"
+
+        // content
+        val textOne = "michael is the best"
+        val textTwo = "michael plays drums"
+
+        val firstDoc : Document = {
+            val doc = new Document()
+            doc.add( new TextField( docIdField, "1", Field.Store.YES ) )
+            doc.add( new TextField( fieldName, textOne, Field.Store.YES ) )
+            doc
+        }
+
+        val secondDoc : Document = {
+            val doc = new Document()
+            doc.add( new TextField( docIdField, "2", Field.Store.YES ) )
+            doc.add( new TextField( fieldName, textTwo, Field.Store.YES ) )
+            doc
+        }
+
+        // write the first doc to the index and wait for the searcher refresh to run in the background
+        index.write( Seq( firstDoc ).asJava )
+        index.refresh() // force the index to read any pending updates
+        val query : Query = new QueryParser( fieldName, new WhitespaceAnalyzer ).parse( "michael" )
+        val firstDocResults = index.search( query )
+        firstDocResults.totalHits shouldBe 1
+
+        // now do the second doc and check
+        index.write( Seq( secondDoc ).asJava )
+        index.refresh() // force a call to tell index to read in updates
+        val incrementalResults = index.search( query ) // reuse the same query
+
+        incrementalResults.totalHits shouldBe 2
+    }
+
+
+}
