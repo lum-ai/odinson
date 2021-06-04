5cad6dfad566f0c998b069d4f5341a3fcb57c32e
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2021-06-04 15:57:12 -0700

    started working on metadata query language

---
+++ core/src/main/scala/ai/lum/odinson/metadata/Ast.scala
@@ -0,0 +1,21 @@
+package ai.lum.odinson.metadata
+
+object Ast {
+
+    sealed trait BoolExpression
+    case class OrExpression(clauses: Seq[BoolExpression]) extends BoolExpression
+    case class AndExpression(clauses: Seq[BoolExpression]) extends BoolExpression
+    case class NotExpression(expr: BoolExpression) extends BoolExpression
+    case class LessThan(lhs: Value, rhs: Value) extends BoolExpression
+    case class LessThanOrEqual(lhs: Value, rhs: Value) extends BoolExpression
+    case class GreaterThan(lhs: Value, rhs: Value) extends BoolExpression
+    case class GreaterThanOrEqual(lhs: Value, rhs: Value) extends BoolExpression
+    case class Equal(lhs: Value, rhs: Value) extends BoolExpression
+    case class NotEqual(lhs: Value, rhs: Value) extends BoolExpression
+
+    sealed trait Value
+    case class NumberValue(n: Int) extends Value
+    case class StringValue(s: String) extends Value
+    case class FunCall(name: String, args: Seq[Value]) extends Value
+
+}
---
+++ core/src/main/scala/ai/lum/odinson/metadata/QueryParser.scala
@@ -0,0 +1,95 @@
+package ai.lum.odinson.metadata
+
+import fastparse._
+import ScriptWhitespace._
+import ai.lum.odinson.compiler.Literals
+
+object MetadataQueryParser {
+
+    def parseQuery(query: String) = {
+        parse(query, top(_))
+    }
+
+    def top[_: P]: P[Ast.BoolExpression] = {
+        or_expression
+    }
+
+    def or_expression[_: P]: P[Ast.BoolExpression] = {
+        P(and_expression.rep(min = 1, sep = "||")).map {
+            case Seq(expr) => expr
+            case expressions => Ast.OrExpression(expressions)
+        }
+    }
+
+    def and_expression[_: P]: P[Ast.BoolExpression] = {
+        P(atomic_expression.rep(min = 1, sep = "&&")).map {
+            case Seq(expr) => expr
+            case expressions => Ast.AndExpression(expressions)
+        }
+    }
+
+    def atomic_expression[_: P]: P[Ast.BoolExpression] = {
+        P(cmp_expression | group_expression)
+    }
+
+    // only a group_expression can be negated,
+    // because we always want parenthesis when applying negation
+    def group_expression[_: P]: P[Ast.BoolExpression] = {
+        P("!".!.? ~ "(" ~ or_expression ~ ")").map {
+            case (None, expr) => expr
+            case (Some(_), expr) => Ast.NotExpression(expr)
+        }
+    }
+
+    def cmp_expression[_: P]: P[Ast.BoolExpression] = {
+        P(value ~ cmp_op ~ value ~ (cmp_op ~ value).rep).map {
+            case (lhs, op, rhs, Seq()) =>
+                mk_compare(lhs, op, rhs)
+            case (lhs, op, rhs, rest) =>
+                var prev = rhs
+                val first = mk_compare(lhs, op, rhs)
+                val clauses = rest.foldLeft(Seq(first)) {
+                    case (left, (op, expr)) =>
+                      val r = mk_compare(prev, op, expr)
+                      prev = expr
+                      left :+ r
+                }
+                clauses match {
+                    case Seq(expr) => expr
+                    case clauses => Ast.AndExpression(clauses)
+                }
+        }
+    }
+
+    def cmp_op[_: P]: P[String] = StringIn("==", "!=", ">", "<", ">=", "<=").!
+
+    def mk_compare(lhs: Ast.Value, op: String, rhs: Ast.Value): Ast.BoolExpression = {
+        op match {
+            case ">" => Ast.GreaterThan(lhs, rhs)
+            case "<" => Ast.LessThan(lhs, rhs)
+            case ">=" => Ast.GreaterThanOrEqual(lhs, rhs)
+            case "<=" => Ast.LessThanOrEqual(lhs, rhs)
+            case "==" => Ast.Equal(lhs, rhs)
+            case "!=" => Ast.NotEqual(lhs, rhs)
+        }
+    }
+
+    def value[_: P]: P[Ast.Value] = {
+        P(funCall | stringValue | numberValue)
+    }
+
+    def funCall[_: P]: P[Ast.Value] = {
+        P(Literals.identifier ~ "(" ~ value.rep(sep = ",") ~ ")").map {
+            case (name, args) => Ast.FunCall(name, args)
+        }
+    }
+
+    def stringValue[_: P]: P[Ast.Value] = {
+        Literals.string.map(Ast.StringValue)
+    }
+
+    def numberValue[_: P]: P[Ast.Value] = {
+        Literals.unsignedInt.map(Ast.NumberValue)
+    }
+
+}
