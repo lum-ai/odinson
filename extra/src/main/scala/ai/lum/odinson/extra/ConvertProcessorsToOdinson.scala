4212f181639c5af0588e4404288b80b44e3c48fe
Becky Sharp <bsharp@email.arizona.edu>
2021-06-04 10:27:34 -0700

    add procExtension and relative paths

--- extra/src/main/scala/ai/lum/odinson/extra/ConvertProcessorsToOdinson.scala
+++ extra/src/main/scala/ai/lum/odinson/extra/ConvertProcessorsToOdinson.scala
@@ -14,14 +14,22 @@ object ConvertProcessorsToOdinson extends App with LazyLogging {
   val config = ConfigFactory.load()
   val docsDir = config.apply[File]("odinson.docsDir")
   val procDir = config.apply[File]("odinson.procDir")
+  val extension = config.apply[String]("odinson.procExtension")
 
   logger.info(s"processors documents at $procDir")
   logger.info(s"odinson documents at $docsDir")
 
-  for (f <- procDir.listFilesByWildcard("*.json", recursive = true).par) {
+  for (f <- procDir.listFilesByWildcard(s"*$extension", recursive = true).par) {
     Try {
-      val newFile = new File(docsDir, f.getName + ".gz")
-      val processorsDoc = JSONSerializer.toDocument(f)
+      val origParent = f.getParent
+      val newDir = new File(origParent.replaceFirst(procDir.toString, docsDir.toString))
+      if (!newDir.exists()) {
+        newDir.mkdirs()
+      }
+      val procName = f.getName
+      val odinsonName = if (procName.endsWith(".gz")) procName else procName + ".gz"
+      val newFile = new File(newDir, odinsonName)
+      val processorsDoc = JSONSerializer.toDocument(f.readString())
       val odinsonDoc = ProcessorsUtils.convertDocument(processorsDoc)
       newFile.writeString(odinsonDoc.toJson)
     } match {
