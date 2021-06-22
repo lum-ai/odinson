bd6687c7aaaa21cb783b7a6fa9e9af2f88d36c83
Keith Alcock <github@keithalcock.com>
2021-06-22 11:06:18 -0700

    Use utils classes an implicits to iterate through Lucene (#306)
    * Use utils classes an implicits to iterate through Lucene
    
    * Unused import
    
    Co-authored-by: Dane Bell <dane@lum.ai>

---
+++ backend/app/utils/LuceneHelpers.scala
@@ -0,0 +1,32 @@
+package utils
+
+object LuceneHelpers {
+
+  type Iteratorable[T] = { def iterator(): T }
+
+  type Nextable[T] = { def next(): T }
+
+  class IteratorFromNextable[T](nextable: Nextable[T]) extends Iterator[T] {
+    protected var nextOpt: Option[T] = Option(nextable.next())
+
+    override def hasNext: Boolean = nextOpt.isDefined
+
+    override def next(): T = {
+      nextOpt.map { next =>
+        nextOpt = Option(nextable.next())
+        next
+      }
+      .getOrElse {
+        throw new RuntimeException("No more elements!")
+      }
+    }
+  }
+
+  implicit class IterableFromNextable[T](nextable: Nextable[T]) extends Iterable[T] {
+    override def iterator: Iterator[T] = new IteratorFromNextable(nextable)
+  }
+
+  implicit class IterableFromIteratorable[T](iteratorable: Iteratorable[Nextable[T]]) extends Iterable[T] {
+    override def iterator: Iterator[T] = iteratorable.iterator().iterator
+  }
+}
