6969cfd9a5984b3761485209b7922ce652846fe0
Michael Reynolds <michael.reynolds@twosixlabs.com>
2021-08-04 13:38:57 -0400

    gardening the tests

--- core/src/test/scala/ai/lum/odinson/lucene/TestIncrementalIndex.scala
+++ core/src/test/scala/ai/lum/odinson/lucene/TestIncrementalIndex.scala
@@ -3,6 +3,7 @@ package ai.lum.odinson.lucene
 import ai.lum.odinson.lucene.index.{IncrementalOdinsonIndex, OdinsonIndex}
 import ai.lum.odinson.utils.IndexSettings
 import ai.lum.odinson.utils.TestUtils.OdinsonTest
+import ai.lum.odinson.utils.exceptions.OdinsonException
 import com.typesafe.config.{Config, ConfigValueFactory}
 import org.scalatest.BeforeAndAfterAll
 
@@ -26,8 +27,6 @@ class TestIncrementalIndex extends OdinsonTest with BeforeAndAfterAll {
     }
 
     "OdinsonIndexWriter" should "object should return index from config correctly" in {
-        //        val indexDir = mkTestIndexDir()
-        //        val config = testConfig.withValue( "odinson.indexDir", ConfigValueFactory.fromAnyRef( indexDir.getAbsolutePath ) )
         val index = createOdinsonIndex( testConfig )
         index.directory.listAll.head shouldBe "write.lock"
         index.close()
@@ -38,25 +37,6 @@ class TestIncrementalIndex extends OdinsonTest with BeforeAndAfterAll {
         dir.deleteRecursively()
     }
 
-
-    it should "replace invalid characters prior to indexing to prevent off-by-one errors" ignore {
-        //        val doc = getDocument( "bad-character" )
-        //
-        //        def ee = mkExtractorEngine( doc )
-        //
-        //        val pattern = "complex <nsubj phosphorylate >dobj []"
-        //        val expectedMatches = Array( "AKT1" )
-        //
-        //        val query = ee.mkQuery( pattern )
-        //        val results = ee.query( query, 1 )
-        //        results.totalHits should equal( 1 )
-        //
-        //        val matches = results.scoreDocs.head.matches
-        //        val docId = results.scoreDocs.head.doc
-        //        val foundStrings = matches.map( m => ee.getStringForSpan( docId, m ) )
-        //
-        //        foundStrings shouldEqual expectedMatches
-    }
 
     it should "properly dump and load relevant settings" in {
         val config = {
@@ -69,91 +49,15 @@ class TestIncrementalIndex extends OdinsonTest with BeforeAndAfterAll {
         settings.storedFields should contain theSameElementsAs Seq( "apple", "banana", "kiwi", index.displayField )
     }
 
-    it should "store stored fields and not others" ignore {
-        //        val doc = getDocument( "rainbows" )
-        //        val customConfig : Config = defaultConfig
-        //          .withValue(
-        //              "odinson.index.storedFields",
-        //              ConfigValueFactory.fromAnyRef( Seq( "tag", "raw" ).asJava )
-        //              )
-        //
-        //        def ee = mkExtractorEngine( customConfig, doc )
-        //
-        //        // we asked it to store `tag` so the extractor engine should be able to access the content
-        //        ee.getTokensForSpan( 0, "tag", 0, 1 ) should contain only "NNS"
-        //        // though `entity` is a field in the Document, it wasn't stored, so the extractor engine shouldn't
-        //        // be able to retrieve the content
-        //        an[ OdinsonException ] should be thrownBy ee.getTokensForSpan( 0, "entity", 0, 1 )
-
-    }
-
     it should "throw an exception if the displayField isn't in the storedFields" ignore {
-        //        val indexFile = new File( tmpFolder, "index2" )
-        //        val customConfig : Config = {
-        //            testConfig
-        //              // re-compute the index and docs path's
-        //              .withValue( "odinson.tmpFolder", ConfigValueFactory.fromAnyRef( indexFile.getAbsolutePath ) )
-        //              .withValue(
-        //                  "odinson.index.storedFields",
-        //                  ConfigValueFactory.fromAnyRef( Seq( "apple", "banana", "kiwi" ).asJava )
-        //                  )
-        //        }
-        //
-        //        an[ OdinsonException ] shouldBe thrownBy {
-        //            OdinsonIndexWriter.fromConfig( customConfig )
-        //        }
-    }
+        val config : Config = {
+            testConfig
+              .withValue( "odinson.index.storedFields", ConfigValueFactory.fromAnyRef( Seq( "apple", "banana", "kiwi" ).asJava ) )
+        }
 
-    it should "store and retrieve large graphs" ignore {
-        //        val numTokens = 4000
-        //        val large = Array.fill[ String ]( numTokens )( "test" )
-        //        large( 0 ) = "start"
-        //        large( 3 ) = "success"
-        //        val raw = TokensField( "raw", large.toSeq )
-        //        val word = TokensField( "word", large.toSeq )
-        //
-        //        val deps = new Array[ (Int, Int, String) ]( numTokens )
-        //        for ( i <- 0 until numTokens - 1 ) {
-        //            if ( i < 3 ) {
-        //                // (0,1,edge)
-        //                deps( i ) = (i, i + 1, "edge")
-        //            } else {
-        //                deps( i ) = (i, i + 1, "nomatch")
-        //            }
-        //        }
-        //        val roots = Set[ Int ]( 0 )
-        //        val graph = GraphField( "dependencies", deps, roots )
-        //
-        //        val sent = Sentence( numTokens, Seq( raw, word, graph ) )
-        //        val doc = Document( "testdoc", Seq.empty, Seq( sent ) )
-        //
-        //        // Yes, in fact the deps field is above the previous threshold
-        //        val incomingEdges = graph.mkIncomingEdges( numTokens )
-        //        val outgoingEdges = graph.mkOutgoingEdges( numTokens )
-        //        val indexWriter = createOdinsonIndex
-        //
-        //        val directedGraph = indexWriter.mkDirectedGraph( incomingEdges, outgoingEdges, roots.toArray )
-        //        val bytes = UnsafeSerializer.graphToBytes( directedGraph )
-        //        // previously, we only supported up to this: sortedDocValuesFieldMaxSize = 32766
-        //        bytes.length should be > ( 32766 )
-        //        indexWriter.close()
-        //
-        //        // ensure the EE can use it
-        //        val ee =
-        //            extractorEngineWithConfigValue( doc, "odinson.index.maxNumberOfTokensPerSentence", numTokens )
-        //        val rules =
-        //            """
-        //              |rules:
-        //              |  - name: testrule
-        //              |    type: basic
-        //              |    label: Test
-        //              |    pattern: |
-        //              |      start >edge+ success
-        //              |""".stripMargin
-        //        val extractors = ee.compileRuleString( rules )
-        //        val results = ee.extractNoState( extractors ).toArray
-        //        results.length should be( 1 )
-        //        ee.getTokensForSpan( results.head ) should contain only "success"
+        an[ OdinsonException ] shouldBe thrownBy {
+            OdinsonIndex.fromConfig( config )
+        }
     }
 
     it should "incrementally write to an index that remains open" in {
