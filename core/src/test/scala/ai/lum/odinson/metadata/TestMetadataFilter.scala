5d85c6bf179fab3110f116b606d6620668391a2c
Becky Sharp <bsharp@email.arizona.edu>
2021-08-05 12:24:13 -0700

    Metadata grammars (#324)
    * Added support for metadata filters in the grammars
    
    along with corresponding tests
    
    * added test of import combining
    
    * imported metadata fix

--- core/src/test/scala/ai/lum/odinson/metadata/TestMetadataFilter.scala
+++ core/src/test/scala/ai/lum/odinson/metadata/TestMetadataFilter.scala
@@ -349,4 +349,69 @@ class TestMetadataFilter extends OdinsonTest {
     ee.query(filteredQuery).scoreDocs.length shouldBe (1)
   }
 
+  "Metadata" should "work when in grammars as a string" in {
+    ee.clearState()
+    val rules = """
+      |metadataFilters: doctype == 'article'
+      |
+      |vars:
+      |  chunk: "[chunk=B-NP][chunk=I-NP]*"
+      |
+      |rules:
+      |  - name: testrule
+      |    type: event
+      |    label: Test
+      |    pattern: |
+      |      trigger = [lemma=eat]
+      |      subject: ^NP = >nsubj ${chunk}
+      |      object: ^NP = >dobj ${chunk}
+    """.stripMargin
+    val extractors = ee.ruleReader.compileRuleString(rules)
+    val mentions = getMentionsWithLabel(ee.extractMentions(extractors).toSeq, "Test")
+    mentions should have size (3)
+
+  }
+
+  "Metadata" should "work when in grammars as a list" in {
+    ee.clearState()
+    val rules = """
+      |metadataFilters:
+      |  - doctype == 'article'
+      |  - (date(1999, 01, 01) < pubdate < date(2012, 01, 01))
+      |
+      |vars:
+      |  chunk: "[chunk=B-NP][chunk=I-NP]*"
+      |
+      |rules:
+      |  - name: testrule
+      |    type: event
+      |    label: Test
+      |    pattern: |
+      |      trigger = [lemma=eat]
+      |      subject: ^NP = >nsubj ${chunk}
+      |      object: ^NP = >dobj ${chunk}
+    """.stripMargin
+    val extractors = ee.ruleReader.compileRuleString(rules)
+    val mentions = getMentionsWithLabel(ee.extractMentions(extractors).toSeq, "Test")
+    mentions should have size (2)
+
+  }
+
+  "Metadata" should "combine properly when files are imported" in {
+    ee.clearState()
+    val masterPath = "/testMetadataImports/master.yml"
+    val extractors = ee.compileRuleResource(masterPath)
+    val mentions = getMentionsWithLabel(ee.extractMentions(extractors).toSeq, "Test")
+    mentions should have size (2)
+
+  }
+
+  "Metadata" should "combine properly when files are imported with filter" in {
+    ee.clearState()
+    val masterPath = "/testMetadataImports/master2.yml"
+    val extractors = ee.compileRuleResource(masterPath)
+    val mentions = getMentionsWithLabel(ee.extractMentions(extractors).toSeq, "Test")
+    mentions should have size (2)
+
+  }
 }
