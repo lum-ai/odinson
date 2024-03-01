15db601f0bac05218b474f0b6dfa7867c32d0c64
Gus Hahn-Powell <gushahnpowell@gmail.com>
2024-03-01 13:13:36 -0700

    Additional test

--- core/src/test/scala/ai/lum/odinson/metadata/TestJoinQueries.scala
+++ core/src/test/scala/ai/lum/odinson/metadata/TestJoinQueries.scala
@@ -33,4 +33,20 @@ class TestJoinQueries extends OdinsonTest {
     numMatches(res) should be (1)
     existsMatchWithSpan(odinResults = res, doc = 0, start = 0, end = 3) should be (true)
   }
+
+  it should "match simple patterns w/o traversals that use exact range quantifiers (>=2) and a metadata filter" in {
+
+    // create in-memory engine w/ a single doc w/ a single sentence
+    val ee = mkExtractorEngine("step-bros")
+    // "John", "C.", "Reilly", "played" ...
+    val pattern = "[tag=NNP]{3} [lemma=play]"
+    val mf = "doc_id == 'step-bros'"
+    val oq = ee.mkFilteredQuery(query = pattern, metadataFilter = mf)
+
+    val res = ee.query(oq)
+    // println(s"match: (${res.scoreDocs.head.matches.head.start}, ${res.scoreDocs.head.matches.head.end})")
+ 
+    numMatches(res) should be (1)
+    existsMatchWithSpan(odinResults = res, doc = 0, start = 0, end = 4) should be (true)
+  }
 }
